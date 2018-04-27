0215e656061ecabaaf7c1f1c143b3702ec481f09
nickywongdong <wongnich@oregonstate.edu>
2018-04-27 00:05:05 -0700

    adding color to text for easy debugging

--- concurrency/concurrency2/solution1.c
+++ concurrency/concurrency2/solution1.c
@@ -9,12 +9,24 @@
 #include <stdint.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <time.h>
 #include "mt19937ar.c"
 
-sem_t footman, forks[5];
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
-void get_forks(int*);
-void put_forks(int*);
+
+sem_t footman, forks[5];
+time_t rawtime;
+struct tm * timeinfo;
+
+void get_forks(void*);
+void put_forks(void*);
 void checkSystem();
 int left(int);
 int right(int);
@@ -28,16 +40,16 @@ int main(int argc, char *argv[]) {
 	//use mt19937:
 	init_genrand(seed);
 
-	void (*get_forks_functions[5])(int *);
+	/*void (*get_forks_functions[5])(int *);
 	void (*put_forks_functions[5])(int *);
 
-	for(i=0; i<5; i++){
+	for(i=0; i<4; i++){
 		get_forks_functions[i] = &get_forks;
 		put_forks_functions[i] = &put_forks;
-	}
+	}*/
 
 	//set up fork semaphores and footman:
-	for(i=0; i<5; i++) {
+	for(i=0; i<4; i++) {
 		sem_init(&forks[i], 0, 1);
 	}
 	sem_init(&footman, 0, 4);
@@ -46,8 +58,10 @@ int main(int argc, char *argv[]) {
 	printf("Beginning dining philosophers simulation... \n  --Press Enter to stop\n");
 
 	for(i=0; i<4; i++){
-			pthread_create(&philosopher[i], NULL, (void *) get_forks_functions[i], (void *) &i);	//grab the fork
-			pthread_create(&philosopher[i], NULL, (void *) put_forks_functions[i], (void *) &i);	//put fork down
+		int *index = malloc(sizeof(*index));
+		*index = i;
+		pthread_create(&philosopher[i], NULL, (void *) get_forks, index);	//grab the fork
+		pthread_create(&philosopher[i], NULL, (void *) put_forks, index);	//put fork down
 	}
 
 	getchar();
@@ -68,13 +82,16 @@ footman . wait ()
 fork [ right (i )]. wait ()
 fork [ left ( i )]. wait ()
 */
-void get_forks(int *i) {
+void get_forks(void *index) {
+	int i = *((int *) index);
 	while(1) {
-	
-		printf("Philosopher %d is getting fork\n", *i);
+		time (&rawtime);
+  		timeinfo = localtime (&rawtime);
+  		printf("%s", asctime(timeinfo));
+		printf(ANSI_COLOR_GREEN "Philosopher %d is getting fork\n" ANSI_COLOR_RESET, i);
 		sem_wait(&footman);
-		sem_wait(&forks[right(*i)]);
-		sem_wait(&forks[left(*i)]);
+		sem_wait(&forks[right(i)]);
+		sem_wait(&forks[left(i)]);
 	}
 }
 
@@ -85,24 +102,42 @@ footman . signal ()
 
 */
 
-void put_forks(int *i){
+void put_forks(void *index){
+	int i = *((int *) index);
 	while(1) {
 		int think_time, eat_time;
 
 		//simulate eating:
 		eat_time = genrand_int32() % (9 + 1 - 2) + 2; //"eating" time from 2 - 9
-		printf("Philosopher %d taking %d seconds to eat\n", *i, eat_time);
+
+  	time (&rawtime);
+  	timeinfo = localtime (&rawtime);
+  	printf("%s", asctime(timeinfo));
+  		printf(ANSI_COLOR_YELLOW "Philosopher %d taking %d seconds to eat\n" ANSI_COLOR_RESET, i, eat_time);
 		sleep(eat_time);
+
+
+  	time (&rawtime);
+  	timeinfo = localtime (&rawtime);
+  	printf("%s", asctime(timeinfo));
+		printf(ANSI_COLOR_BLUE "Philosopher %d is done eating, putting fork down\n" ANSI_COLOR_RESET , i);
 	
-		printf("Philosopher %d is putting fork down\n", *i);
-		sem_post(&forks[right(*i)]);
-		sem_post(&forks[left(*i)]);
+		sem_post(&forks[right(i)]);
+		sem_post(&forks[left(i)]);
 		sem_post(&footman);
 	
 		//simulate thinking:
 		think_time = genrand_int32() % (20 + 1 - 1) + 1; //"thinking" time from 1 - 20
-		printf("Philosopher %d will think for %d seconds\n", *i, think_time);
+
+		time (&rawtime);
+  		timeinfo = localtime (&rawtime);
+			printf(ANSI_COLOR_MAGENTA "Philosopher %d will think for %d seconds\n" ANSI_COLOR_RESET, i, think_time);
 		sleep(think_time);
+
+ 	 	time (&rawtime);
+  		timeinfo = localtime (&rawtime);
+		printf(ANSI_COLOR_CYAN "Philosopher %d is done thinking\n" ANSI_COLOR_RESET, i);
+		//sleep(50000);
 	}
 }
 
