9d095fb7c73dbfb89b40f5bf86368a00f4a36825
nickywongdong <wongnich@oregonstate.edu>
2018-04-29 20:50:44 -0700

    finished assignment

--- concurrency/concurrency2/table.c
+++ concurrency/concurrency2/table.c
@@ -10,6 +10,7 @@
 #include <semaphore.h>
 #include <fcntl.h>
 #include <time.h>
+#include <termios.h>
 #include "mt19937ar.c"
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -19,20 +20,21 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_BOLD_GREEN	   "\x1b[1;32m"
 
 
 sem_t footman, forks[5];
 int philosopher_status[5], forks_status[5], philosopher_sleep_time[5];
 char philosophers[5][128];
 
-void get_forks(void*);
-void put_forks(void*);
-void philosopher_thread(void*);
+void get_forks(void *);
+void put_forks(void *);
+void philosopher_thread(void *);
 void checkSystem();
 void print_table();
 int left(int);
 int right(int);
-
+int getkey();
 
 int main(int argc, char *argv[]) {
 	int i, seed = time(NULL);
@@ -48,29 +50,31 @@ int main(int argc, char *argv[]) {
 	}
 	sem_init(&footman, 0, 4);
 
-	strcpy(philosophers[0], "Kevin");
+	//name the philosophers
+	strcpy(philosophers[0], "Kevin");		// best one there is
 	strcpy(philosophers[1], "Socrates");
 	strcpy(philosophers[2], "Descartes");
 	strcpy(philosophers[3], "Plato");
 	strcpy(philosophers[4], "Aristotle");
 	
 
-	printf("Beginning dining philosophers simulation...\n");
-
+	//initialize threads
 	for(i=0; i<5; i++){
 		int *index = malloc(sizeof(*index));
 		*index = i;
-		pthread_create(&philosopher[i], NULL, (void *) philosopher_thread, index); //begin thread for each philosopher
+		pthread_create(&philosopher[i], NULL, (void *) philosopher_thread, index);
 	}
 
-	while(1) {
+
+	//print the table
+	while(getkey() != 27) {
+		printf("\e[1;1H\e[2J");
 		print_table();
 		sleep(1);
-		printf("\e[1;1H\e[2J");
 	}
-	//getchar();
 
-	printf("cleaning up...\n");
+
+	printf("ESC char received, terminating now...\n");
 	//clean up:
 	sem_destroy(&footman);
 	for(i=0; i<4; i++){
@@ -146,46 +150,6 @@ void put_forks(void *index){
 	
 }
 
-void print_table() {
-	int i;
-
-	printf("%-20s|%-10s|%-10s|%-10s|%-20s|\n", "Name", "State", "Left Fork", "Right Fork", "Sleep Time");
-	printf("------------------------------------------------------------------------------------------\n");
-
-	for(i = 0; i < 5; i++) {
-		printf("%-20s|", philosophers[i]);
-
-		if(philosopher_status[i] == 0) {
-			printf("%-10s|", "Thinking");
-		} else {
-			printf("%-10s|", "Eating");
-		}
-
-		if(forks_status[left(i)] == 0) {
-			printf("%-10s|", "Unused");
-		} else {
-			printf("%-10s|", "Used");
-		}
-
-		if(forks_status[right(i)] == 0) {
-			printf("%-10s|", "Unused");
-		} else {
-			printf("%-10s|", "Used");
-		}
-
-		if(philosopher_sleep_time[i] >=0 ) {
-			printf("%-20d|", philosopher_sleep_time[i]);
-		} else {
-			printf("%-20s|", "Waiting on Forks");
-		}
-		philosopher_sleep_time[i] = philosopher_sleep_time[i] - 1;
-
-
-		printf("\n");
-	}
-
-}
-
 
 void checkSystem() {
         unsigned int eax;
@@ -223,3 +187,68 @@ int right(int i) {
 	return ( i + 1 ) % 5;
 }
 
+void print_table() {
+	int i;
+	printf("Dining Philosopher's Simulation - Press " ANSI_COLOR_CYAN "Escape" ANSI_COLOR_RESET " to quit...\n\n");
+	printf("%-20s|%-10s|%-10s|%-10s|%-20s|\n", "Philosopher", "State", "Left Fork", "Right Fork", "Sleep Time");
+	printf("------------------------------------------------------------------------------------------\n");
+
+	for(i = 0; i < 5; i++) {
+		printf("%-20s|", philosophers[i]);
+
+		if(philosopher_status[i] == 0) {
+			printf("%-10s|", "Thinking");
+		} else {
+			printf(ANSI_COLOR_BLUE "%-10s" ANSI_COLOR_RESET "|", "Eating");
+		}
+
+		if(forks_status[left(i)] == 0) {
+			printf("%-10s|", "Unused");
+		} else {
+			printf(ANSI_COLOR_MAGENTA "%-10s" ANSI_COLOR_RESET "|", "Used");
+		}
+
+		if(forks_status[right(i)] == 0) {
+			printf("%-10s|", "Unused");
+		} else {
+			printf(ANSI_COLOR_MAGENTA "%-10s" ANSI_COLOR_RESET "|", "Used");
+		}
+
+		if(philosopher_sleep_time[i] >=0 ) {
+			printf("%-20d|", philosopher_sleep_time[i]);
+		} else {
+			printf(ANSI_COLOR_RED "%-20s" ANSI_COLOR_RESET "|", "Waiting on Forks");
+		}
+		philosopher_sleep_time[i] = philosopher_sleep_time[i] - 1;
+
+
+		printf("\n");
+	}
+
+}
+
+
+// Sourced from JetsonHacks: http://www.jetsonhacks.com/2015/12/29/gpio-interfacing-nvidia-jetson-tx1/
+// Simple function for non-blocking input read
+int getkey() {
+    int character;
+    struct termios orig_term_attr;
+    struct termios new_term_attr;
+
+    /* set the terminal to raw mode */
+    tcgetattr(fileno(stdin), &orig_term_attr);
+    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+    new_term_attr.c_lflag &= ~(ECHO|ICANON);
+    new_term_attr.c_cc[VTIME] = 0;
+    new_term_attr.c_cc[VMIN] = 0;
+    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+
+    /* read a character from the stdin stream without blocking */
+    /*   returns EOF (-1) if no character is available */
+    character = fgetc(stdin);
+
+    /* restore the original terminal attributes */
+    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+
+    return character;
+}
