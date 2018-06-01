9130eaa24a1e4d102533d3a002d88f4fad8d02cc
nickywongdong <wongnich@oregonstate.edu>
2018-05-31 21:16:02 -0700

    think part 2 is done? need to do more testing to be sure

--- concurrency/concurrency4/concurrency4_2.c
+++ concurrency/concurrency4/concurrency4_2.c
@@ -30,60 +30,87 @@ typedef struct
 
 }Lightswitch;
 
+
+
 void print_table();
 int getkey();
-void lock(Lightswitch *, sem_t *);
-void unlock(Lightswitch *, sem_t *);
-void searcher(void *);
-void inserter(void *);
-void deleter(void *);
+void pusher_a();
+void pusher_b();
+void pusher_c();
+void agent_a();
+void agent_b();
+void agent_c();
+void smoker_tobacco();
+void smoker_matches();
+void smoker_paper();
+void makeCigarette();
+void smoke();
 
-sem_t insertMutex, noSearcher, noInserter;
-Lightswitch searchSwitch, insertSwitch;
-int search_status[2], insert_status[2], delete_status[2];
-int search_time[2], insert_time[2], delete_time[2];
+/*
+agentSem = Semaphore (1)
+2 tobacco = Semaphore (0)
+3 paper = Semaphore (0)
+4 match = Semaphore (0)
+*/
+/*
+
+isTobacco = isPaper = isMatch = False
+2 tobaccoSem = Semaphore (0)
+3 paperSem = Semaphore (0)
+4 matchSem = Semaphore (0)
+*/
+
+sem_t tobaccoSem, paperSem, matchSem;
+sem_t tobacco, paper, match, agentSem;
+sem_t mutex;
+int isTobacco, isPaper, isMatch;
+int agent_status[3], resource_wait[3], smoker_status[3], smoker_smoke_time[3], cigarette_craft_time[3];
 
 int main(int argc, char *argv[]) 
 {
 	int i, seed = time(NULL);
-	int *index = malloc(sizeof(*index));
-
-
-	pthread_t searcher_thread1, inserter_thread1, deleter_thread1;
-	pthread_t searcher_thread2, inserter_thread2, deleter_thread2;
-
 	init_genrand(seed);
 
-	//initialize variables for visualization:
-	for(i=0; i<2; i++) {
-		search_status[i] = 0;
-		insert_status[i] = 0;
-		delete_status[i] = 0;
-		search_time[i] = 0;
-		insert_time[i] = 0;
-		delete_time[i] = 0;
+	pthread_t pusher_a_thread, pusher_b_thread, pusher_c_thread;
+	pthread_t agent_a_thread, agent_b_thread, agent_c_thread;
+	pthread_t smoker_paper_thread, smoker_matches_thread, smoker_tobacco_thread;
+
+
+	//initialize semaphores:
+	sem_init(&tobaccoSem, 0, 0);
+	sem_init(&paperSem, 0, 0);
+	sem_init(&matchSem, 0, 0);
+	sem_init(&tobacco, 0, 0);
+	sem_init(&paper, 0, 0);
+	sem_init(&match, 0, 0);
+	sem_init(&agentSem, 0, 1);
+	sem_init(&mutex, 0, 1);
+
+	//initialize booleans for check
+	isTobacco = 0;
+	isPaper = 0;
+	isMatch = 0;
+
+	//variables used for visualization in print_table
+	for(i=0; i<3; i++) {
+		agent_status[i] = 0;
+		resource_wait[i] = 0;
+		smoker_status[i] = 0;
+		smoker_smoke_time[i] = 0;
+		cigarette_craft_time[i] = 0;
 	}
 
-	//initialize semaphores and lightswitches:
-	sem_init(&insertMutex, 0, 1);
-	sem_init(&noSearcher, 0, 1);
-	sem_init(&noInserter, 0, 1);
+	pthread_create(&pusher_a_thread, NULL, (void *) pusher_a, NULL);
+	pthread_create(&pusher_b_thread, NULL, (void *) pusher_b, NULL);
+	pthread_create(&pusher_c_thread, NULL, (void *) pusher_c, NULL);
 
-		searchSwitch.counter = 0;
-		sem_init(&searchSwitch.mutex, 0, 1);
-		insertSwitch.counter = 0;
-		sem_init(&insertSwitch.mutex, 0, 1);
+	pthread_create(&agent_a_thread, NULL, (void *) agent_a, NULL);
+	pthread_create(&agent_b_thread, NULL, (void *) agent_b, NULL);
+	pthread_create(&agent_c_thread, NULL, (void *) agent_c, NULL);
 
-
-	*index = 0;
-	pthread_create(&searcher_thread1, NULL, (void *) searcher, index);
-	pthread_create(&inserter_thread1, NULL, (void *) inserter, index);
-	pthread_create(&deleter_thread1, NULL, (void *) deleter, index);
-
-	*index = 1;
-	pthread_create(&searcher_thread2, NULL, (void *) searcher, index);
-	pthread_create(&inserter_thread2, NULL, (void *) inserter, index);
-	pthread_create(&deleter_thread2, NULL, (void *) deleter, index);
+	pthread_create(&smoker_paper_thread, NULL, (void *) smoker_paper, NULL);
+	pthread_create(&smoker_matches_thread, NULL, (void *) smoker_matches, NULL);
+	pthread_create(&smoker_tobacco_thread, NULL, (void *) smoker_tobacco, NULL);
 
 	//print the table
 	while(getkey() != 27) {
@@ -92,146 +119,190 @@ int main(int argc, char *argv[])
 		sleep(1);
 	}
 
+
 	printf("ESC char received, terminating now...\n");
+
 	//clean up:
-	sem_destroy(&insertMutex);
-    sem_destroy(&noSearcher);
-    sem_destroy(&noInserter);
-
-    	sem_destroy(&searchSwitch.mutex);
-    	sem_destroy(&insertSwitch.mutex);
-
+	sem_destroy(&tobaccoSem);
+	sem_destroy(&paperSem);
+	sem_destroy(&matchSem);
+	sem_destroy(&tobacco);
+	sem_destroy(&paper);
+	sem_destroy(&match);
+	sem_destroy(&agentSem);
+	sem_destroy(&mutex);
 
 	return 0;
 }
 
-
-
-//searcher
 /*
-searchSwitch . wait ( noSearcher )
-//critical section
-searchSwitch . signal ( noSearcher )
+agentSem . wait ()
+2 tobacco . signal ()
+3 paper . signal ()
 */
-void searcher(void *index)
+void agent_a()
 {
-	int i = *((int *) index);
 	while(1) {
-		lock(&searchSwitch, &noSearcher);
-		
-		// critical section
-		search_status[i] = 1;	//signal that searcher is running
-		search_time[i] = genrand_int32() % (5 + 1 - 1) + 1; //"consume" time from 1 - 5
-
-		sleep(search_time[i]);				//emulate a search time
-
-		unlock(&searchSwitch, &noSearcher);
-
-		search_status[i] = 0;
-
-		sleep(5);		//allow other operations to take place
-	}
-}
-
-
-//inserter
-/*
-insertSwitch . wait ( noInserter )
-insertMutex . wait ()
-// critical section
-insertMutex . signal ()
-insertSwitch . signal ( noInserter )
-*/
-void inserter(void *index)
-{
-	int i = *((int *) index);
-	while(1) {
-		lock(&insertSwitch, &noInserter);
-		sem_wait(&insertMutex);
-
-		//critical section:
-		insert_status[i] = 1;
-		insert_time[i] = genrand_int32() % (5 + 1 - 1) + 1; //"consume" time from 1 - 5
-		sleep(insert_time[i]);
-
-		sem_post(&insertMutex);
-		unlock(&insertSwitch, &noInserter);
-		insert_status[i] = 0;
-
-		sleep(5);		//allow other operations to take place
-
-	}
-
-}
-
-//deleter
-/*
-noSearcher . wait ()
-noInserter . wait ()
-//critical section
-noInserter . signal ()
-noSearcher . signal ()
-*/
-void deleter(void *index)
-{
-	int i = *((int *) index);
-	while(1) {
-		sem_wait(&noSearcher);
-		sem_wait(&noInserter);
-
-		//critical section:
-		delete_status[i] = 1;
-		delete_time[i] = genrand_int32() % (5 + 1 - 1) + 1; //"consume" time from 1 - 5
-		sleep(delete_time[i]);
-
-		sem_post(&noInserter);
-		sem_post(&noSearcher);
-
-		delete_status[i] = 0;
-
-		sleep(5);		//allow other operations to take place
-
+		//printf("int agent a, waiting on agentSem\n");
+		sem_wait(&agentSem);
+		sem_post(&tobacco);
+		sem_post(&paper);
 	}
 }
 
 /*
-Lightswitch Implementation:
-class Lightswitch :
-def __init__ ( self ):
-self . counter = 0
-self . mutex = Semaphore (1)
-def lock ( self , semaphore ):
-self . mutex . wait ()
-self . counter += 1
-if self . counter == 1:
- semaphore . wait ()
- self . mutex . signal ()
-
- def unlock ( self , semaphore ):
- self . mutex . wait ()
- self . counter -= 1
- if self . counter == 0:
- semaphore . signal ()
- self . mutex . signal ()*/
-
-void lock(Lightswitch *self, sem_t *semaphore)
+agentSem . wait ()
+2 paper . signal ()
+3 match . signal ()
+*/
+void agent_b()
 {
-    sem_wait(&self->mutex);
-    self->counter+=1;
-    if (self->counter == 1) {
-        sem_wait(semaphore);
-    }
-    sem_post(&self->mutex);
+	while(1) {
+		//printf("int agent b, waiting on agentSem\n");
+		sem_wait(&agentSem);
+		sem_post(&paper);
+		sem_post(&match);
+	}
 }
 
-void unlock(Lightswitch *self, sem_t* semaphore)
+/*
+agentSem . wait ()
+2 tobacco . signal ()
+3 match . signal ()*/
+void agent_c()
 {
-    sem_wait(&self->mutex);
-    self->counter-=1;
-    if (self->counter == 0) {
-         sem_post(semaphore);
-    }
-    sem_post(&self->mutex);
+	while(1) {
+		//printf("int agent c, waiting on agentSem\n");
+		sem_wait(&agentSem);
+		sem_post(&tobacco);
+		sem_post(&match);
+	}
+	
+}
+
+
+/*
+tobacco . wait ()
+2 mutex . wait ()
+3 if isPaper :
+4 isPaper = False
+5 matchSem . signal ()
+6 elif isMatch :
+7 isMatch = False
+8 paperSem . signal ()
+9 else :
+10 isTobacco = True
+11 mutex . signal ()
+*/
+void pusher_a()
+{
+	while(1) {
+		sem_wait(&tobacco);
+		sem_wait(&mutex);
+		if (isPaper) {
+			isPaper = 0;
+			sem_post(&matchSem);
+		} else if (isMatch) {
+			isMatch = 0;
+			sem_post(&paperSem);
+		} else {
+			isTobacco = 1;
+		}
+		sem_post(&mutex);
+	}
+}
+
+void pusher_b()
+{
+	while(1) {
+		sem_wait(&paper);
+		sem_wait(&mutex);
+		if (isTobacco) {
+			isTobacco = 0;
+			sem_post(&matchSem);
+		} else if (isMatch) {
+			isMatch = 0;
+			sem_post(&tobaccoSem);
+		} else {
+			isPaper = 1;
+		}
+		sem_post(&mutex);
+	}
+}
+
+void pusher_c()
+{
+	while(1) {
+		sem_wait(&match);
+		sem_wait(&mutex);
+		if (isTobacco) {
+			isTobacco = 0;
+			sem_post(&paperSem);
+		} else if (isPaper) {
+			isPaper = 0;
+			sem_post(&tobaccoSem);
+		} else {
+			isMatch = 1;
+		}
+		sem_post(&mutex);
+	}
+}
+
+void smoker_matches()
+{
+	while(1) {
+		sem_wait(&matchSem);
+		makeCigarette(0);
+		sem_post(&agentSem);
+		smoke(0);
+	}
+}
+
+void smoker_paper()
+{
+	while(1) {
+		sem_wait(&paperSem);
+		makeCigarette(1);
+		sem_post(&agentSem);
+		smoke(1);
+	}
+}
+/*
+tobaccoSem . wait ()
+2 makeCigarette ()
+3 agentSem . signal ()
+4 smoke ()
+*/
+void smoker_tobacco()
+{
+	while(1) {
+		sem_wait(&tobaccoSem);
+		makeCigarette(2);
+		sem_post(&agentSem);
+		smoke(2);
+	}
+}
+
+//smoker will call this function when he receives all necessary items
+void makeCigarette(int i)
+{	
+	smoker_status[i] = 1;		//smoker status is now crafting cigarette
+	//take between 1-5 seconds to make a cigarette;
+	int rand_craft_time = genrand_int32() % (5 + 1 - 1) + 1;
+	cigarette_craft_time[i] = rand_craft_time;
+	sleep(rand_craft_time);
+}
+
+//smoker will call this function once he has made a cigarette, and will spend some time in here
+void smoke(int i)
+{
+	smoker_status[i] = 2;		//smoker status is now smoking
+	//take 10-15 seconds to smoke a cigarette;
+	int rand_smoke_time = genrand_int32() % (15 + 1 - 10) + 10;
+	smoker_smoke_time[i] = rand_smoke_time;
+	sleep(rand_smoke_time);
+	smoker_status[i] = 0;		//smoker goes back to waiting for resources
+
 }
 
 // Sourced from JetsonHacks: http://www.jetsonhacks.com/2015/12/29/gpio-interfacing-nvidia-jetson-tx1/
@@ -263,65 +334,41 @@ int getkey() {
 void print_table() 
 {
 	int i;
-	printf("Search-Insert-Delete Simulation - Press " ANSI_COLOR_CYAN "Escape" ANSI_COLOR_RESET " to quit...\n\n");
-	printf("%-10s|%-20s|%-20s|\n", "Thread", "Status", "Operation Time");
-	printf("------------------------------------------------------------\n");
+	printf("Cigarette Smokers Simulation - Press " ANSI_COLOR_CYAN "Escape" ANSI_COLOR_RESET " to quit...\n\n");
+	printf("%-10s|%-20s|%-20s|\n", "Smoker", "Status", "Sleep Time");
+	printf("--------------------------------------------------------\n");
 
-		for(i=0; i<2; i++) {
-			printf("%-10s %d|", "Search:", i);
+	for(i=0; i<3; i++) {
+		printf("%-10d|", i);
 
-			if(search_status[i] == 0) {
-				printf("%-20s|", "Waiting");
-			} else if (search_status[i] == 1 ) {
-				printf(ANSI_COLOR_BLUE "%-20s" ANSI_COLOR_RESET "|", "Using Resource");
-			}
+		if(smoker_status[i] == 0) {
+			printf("%-20s|", "Waiting");
 
-			if(search_time[i] > 0 ) {
-				printf("%-20d|", search_time[i]);
+		} else if (smoker_status[i] == 1 ) {
+			printf(ANSI_COLOR_BLUE "%-20s" ANSI_COLOR_RESET "|", "Crafting Cigarette");
+			
+			//display wait time
+			if(cigarette_craft_time[i] > 0 ) {
+				printf("%-20d|", cigarette_craft_time[i]);
+			cigarette_craft_time[i]--;
+
 			} else {
 				printf(ANSI_COLOR_RED "%-20s" ANSI_COLOR_RESET "|", "Waiting");
 			}
-			search_time[i] = search_time[i] - 1;
 
-			printf("\n");
-		}
+		} else if (smoker_status[i] == 2 ) {
+			printf(ANSI_COLOR_BLUE "%-20s" ANSI_COLOR_RESET "|", "Smoking");
 
-		for(i=0; i<2; i++) {
-			printf("%-10s %d|", "Insert:", i);
+			//display wait time
+			if(smoker_smoke_time[i] > 0 ) {
+				printf("%-20d|", smoker_smoke_time[i]);
+				smoker_smoke_time[i]--;
 
-			if(insert_status[i] == 0) {
-				printf("%-20s|", "Waiting");
-			} else if (insert_status[i] == 1 ) {
-				printf(ANSI_COLOR_BLUE "%-20s" ANSI_COLOR_RESET "|", "Using Resource");
-			}
-
-			if(insert_time[i] > 0 ) {
-				printf("%-20d|", insert_time[i]);
 			} else {
 				printf(ANSI_COLOR_RED "%-20s" ANSI_COLOR_RESET "|", "Waiting");
 			}
-			insert_time[i] = insert_time[i] - 1;
-
-			printf("\n");
 		}
 
-		for(i=0; i<2; i++) {
-			printf("%-10s %d|", "Delete:", i);
-
-			if(delete_status[i] == 0) {
-				printf("%-20s|", "Waiting");
-			} else if (delete_status[i] == 1 ) {
-				printf(ANSI_COLOR_BLUE "%-20s" ANSI_COLOR_RESET "|", "Using Resource");
-			}
-
-			if(delete_time[i] > 0 ) {
-				printf("%-20d|", delete_time[i]);
-			} else {
-				printf(ANSI_COLOR_RED "%-20s" ANSI_COLOR_RESET "|", "Waiting");
-			}
-			delete_time[i] = delete_time[i] - 1;
-
-			printf("\n");
-		}
-
+		printf("\n");
+	}
 }
